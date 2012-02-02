9436c8097fd22fed8ff99438bf3a7ed0f8fe3a97
rednaks <rednaks@pro-programming.net>
2012-02-02 15:19:01 +0100

    dialog + debug

--- src/content/browserOverlay.js
+++ src/content/browserOverlay.js
@@ -21,8 +21,8 @@ XULMzTnChrome.BrowserOverlay = {
     var tab_title = gBrowser.selectedTab.getAttribute('label');
     var url = gBrowser.currentURI.spec;
     var twitter_req = "https://twitter.com/intent/tweet?original_referer=&source=TweetMozAddon&text="+encodeURI(tab_title)+"&url="+encodeURI(url)+"&via=MozillaTunisia";
-    window.alert(twitter_req);
-    //window.openDialog(twitter_req); 
-    openUILinkIn(twitter_req,"tab");
+    //window.alert(twitter_req);
+    window.openDialog(twitter_req); 
+    //openUILinkIn(twitter_req,"tab");
   }
 };
